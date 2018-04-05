6f5c3f537768faa73e94ea37974fd91b9021cb1f
Ramzi Dreessen <ramzidreessen@gmail.com>
2018-04-04 19:40:35 -0500

    hooked up hotels API with display

--- assets/main.js
+++ assets/main.js
@@ -17,44 +17,51 @@
         console.log(long);
 
         //hotel data pull with lat and long variable included
-            $.ajax({
-                url: "https://api.sandbox.amadeus.com/v1.2/hotels/search-circle?apikey=yYOQMsJDsfO5Yk0WU7xgP6vX7YS8H1lC&latitude=" + lat + "&longitude=" + long + "&radius=10&check_in=2018-06-15&check_out=2018-06-16&currency=USD&number_of_results=4",
-                method: "GET"
-            }).then(function(response) {
-                console.log(response);
+        $.ajax({
+            url: "https://api.sandbox.amadeus.com/v1.2/hotels/search-circle?apikey=yYOQMsJDsfO5Yk0WU7xgP6vX7YS8H1lC&latitude=" + lat + "&longitude=" + long + "&radius=10&check_in=2018-06-15&check_out=2018-06-16&currency=USD&number_of_results=6",
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
 
-                console.log(response.results["0"].property_name);
-                console.log(response.results["0"].amenities["0"].description);
-                console.log("http://hotelsearchengine.amadeus.com/hotelSearchEngineBrowser/#SINGLE/propertyCode="+response.results["0"].property_code +"")
-
-                let stayItem = $("<a>").attr("href", "http://hotelsearchengine.amadeus.com/hotelSearchEngineBrowser/#SINGLE/propertyCode="+response.results[1].property_code +"");
-                let stayItemTitle = $("<h2>").text(response.results["0"].property_name);
-                let stayItemDesc = $("<p>").text(response.results["0"].amenities);
-                stayItem.append(stayItemTitle).append(stayItemDesc);
-            });    
+            console.log(response.results["0"].property_name);
+            console.log(response.results["0"].amenities["0"].description);
+            console.log(response.results["0"].total_price.amount);
+            console.log("http://hotelsearchengine.amadeus.com/hotelSearchEngineBrowser/#SINGLE/propertyCode="+response.results["0"].property_code +"")
+            
+            $('.js-hotels').empty();
+            for (let i = 0; i < response.results.length; i++) {
+                let stayItem = $("<a>").attr("href", "http://hotelsearchengine.amadeus.com/hotelSearchEngineBrowser/#SINGLE/propertyCode="+response.results[i].property_code +"");
+                let stayItemPrice = $("<h3>").text("$" + response.results[i].total_price.amount + "/night");
+                let stayItemTitle = $("<h2>").text(response.results[i].property_name);
+                let stayItemDesc = $("<p>").text(response.results[i].amenities[0].description + ", " + response.results[i].amenities[1].description);
+                stayItem.append(stayItemPrice).append(stayItemTitle).append(stayItemDesc);
+                $('.js-hotels').append(stayItem);
+            }
+        });    
 
         //weather data pull using city
-            $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/weather?q=" + place +"&appid=9ba90c3ac88ea1ab90059c06b34a2ce1",
-                method: "GET"
-            }).then(function(response) {    
-                console.log(response);
-                var valNum = response.main.temp
-                valNum = parseFloat(valNum);
-                var tempF = Math.trunc(((valNum-273.15)*1.8)+32);
-                $(".js-weather-temp").text(tempF);         
-                $(".js-weather-description").text(response.weather["0"].description);      
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + place +"&appid=9ba90c3ac88ea1ab90059c06b34a2ce1",
+            method: "GET"
+        }).then(function(response) {    
+            console.log(response);
+            var valNum = response.main.temp
+            valNum = parseFloat(valNum);
+            var tempF = Math.trunc(((valNum-273.15)*1.8)+32);
+            $(".js-weather-temp").text(tempF);         
+            $(".js-weather-description").text(response.weather["0"].description);      
 
-            });    
+        });    
+
         //restaurants data pull using city, lat, long
-            $.ajax({
-                url: "https://api.barzz.net/api/search?city=chicago&state=IL&user_key=2de014817647c19e1bd4a957864fabe5",
-                method: "GET",
-                crossDomain: true,
-                dataType: 'json',
-            }).then(function(response) {    
-                console.log(response);
-            });    
+        $.ajax({
+            url: "https://api.barzz.net/api/search?city=chicago&state=IL&user_key=2de014817647c19e1bd4a957864fabe5",
+            method: "GET",
+            crossDomain: true,
+            dataType: 'json',
+        }).then(function(response) {    
+            console.log(response);
+        });    
       });
 
   });
